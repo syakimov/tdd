ab5441ffa3ad1f0fc4c555ea3765c877587b6fcc
Stefan Yakimov <stefan.yakimov@receipt-bank.com>
2019-01-21 18:32:56 +0200

    Complete third spec

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_18_133117) do
+ActiveRecord::Schema.define(version: 2019_01_21_162411) do
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
--- spec/features/03_user_sees_own_todos_spec.rb
+++ spec/features/03_user_sees_own_todos_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 feature 'User sess own todos' do
-  skip scenario "user does not see others' todos" do
+  scenario "user does not see others' todos" do
     Todo.create!(title: 'Todo Title', email: 'someone_else@example.com')
 
     sign_in_as 'me@example.com'
